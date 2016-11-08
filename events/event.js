9bf2a05461925711966f2386924787381a35f7e5
GitDwarf <spencerbog1@gmail.com>
2016-11-08 16:05:50 +1300

    Update event.js

--- events/event.js
+++ events/event.js
@@ -1,13 +1,12 @@
-class event{
-    constructor(name, id, creator_name){
-        this.name = name;
-        this.id = id;
-        this.creator_name = creator_name;
-        this.players = [];
-    }
-    addPlayer(player){
-        this.players.push(player);
-        console.log('Event: '+this.id+", adding player: "+player);
-    }
+
+function Event(id, name){
+    this.players = [];
+    this.id = id;
+    this.name = name;
 }
-module.exports = event;
+function addPlayer(player){
+    this.players.push(player);
+    console.log('Event: '+this.id+", adding player: "+player);
+}
+
+
