2a1d8644aba317e60e53ef1ea1f490e3c1ffd003
NullRoz007 <spencerbog1@gmail.com>
2016-11-08 17:25:10 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,6 +56,7 @@ client.on('message', message => {
 					
 					while(spacedName == false && lookingForSpacedName){
 						for(index = 2; index < splitMessage.length; index++){
+							console.log(splitMessage[index].charAt[0]);
 							if(splitMessage[index].charAt[0] == '"'){
 								spacedName = true;
 								lookingForEnd = true;
