2b1fae91efc28f3999cea33ff3a5eebf751ca2bd
NullRoz007 <spencerbog1@gmail.com>
2016-11-17 21:44:22 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,12 +166,13 @@ client.on('message', message => {
 			}
 		}
 	    	else if(splitMessage[0] === "!clearuser"){
-			if(splitMessage.length === 3){
+			if(splitMessage.length == 3){
 				var name = splitMessage[1];
 				var amount = splitMessage[2];
 				if(hasModPerms(message)){
 					var messagePromise = message.channel.fetchMessages({limit: amount});
 					messagePromise.then(function (pastMsgs) {
+						console.log(messagePromise);
 						for(i = 0; i < pastMsgs.array().length; i++){
 							var msg = pastMsgs.array()[i];
 							var msgUsr = String(msg.member.user.username);
